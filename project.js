baff82f899a059231084b4985f6c8f0338f1582d
Aniket Sabale <85133669+AniketSabale1628@users.noreply.github.com>
2023-08-05 18:11:41 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,500 @@
+let courses=[
+    {
+      "id": 1,
+      "foodname": "Apple",
+      "calorie": 95,
+      "category": "Fruit",
+      "protiens": 0.3,
+      "cab": 25
+    },
+    {
+      "id": 2,
+      "foodname": "Banana",
+      "calorie": 105,
+      "category": "Fruit",
+      "protiens": 1.3,
+      "cab": 27
+    },
+    {
+      "id": 3,
+      "foodname": "Orange",
+      "calorie": 62,
+      "category": "Fruit",
+      "protiens": 1.2,
+      "cab": 15
+    },
+    {
+      "id": 4,
+      "foodname": "Pear",
+      "calorie": 95,
+      "category": "Fruit",
+      "protiens": 0.5,
+      "cab": 25
+    },
+    {
+      "id": 5,
+      "foodname": "Grapefruit",
+      "calorie": 42,
+      "category": "Fruit",
+      "protiens": 1.1,
+      "cab": 11
+    },
+    {
+      "id": 6,
+      "foodname": "Strawberry",
+      "calorie": 46,
+      "category": "Fruit",
+      "protiens": 1.5,
+      "cab": 10
+    },
+    {
+      "id": 7,
+      "foodname": "Blueberry",
+      "calorie": 52,
+      "category": "Fruit",
+      "protiens": 1.1,
+      "cab": 14
+    },
+    {
+      "id": 8,
+      "foodname": "Raspberry",
+      "calorie": 52,
+      "category": "Fruit",
+      "protiens": 1.4,
+      "cab": 15
+    },
+    {
+      "id": 9,
+      "foodname": "Broccoli",
+      "calorie": 34,
+      "category": "Vegetable",
+      "protiens": 3.3,
+      "cab": 5
+    },
+    {
+      "id": 10,
+      "foodname": "Cauliflower",
+      "calorie": 25,
+      "category": "Vegetable",
+      "protiens": 2.6,
+      "cab": 5
+    },
+    {
+      "id": 11,
+      "foodname": "Green Beans",
+      "calorie": 31,
+      "category": "Vegetable",
+      "protiens": 2.4,
+      "cab": 4
+    },
+    {
+      "id": 12,
+      "foodname": "Asparagus",
+      "calorie": 20,
+      "category": "Vegetable",
+      "protiens": 2.2,
+      "cab": 2
+    },
+    {
+      "id": 13,
+      "foodname": "Spinach",
+      "calorie": 23,
+      "category": "Vegetable",
+      "protiens": 3.5,
+      "cab": 4
+    },
+    {
+      "id": 14,
+      "foodname": "Kale",
+      "calorie": 33,
+      "category": "Vegetable",
+      "protiens": 4.3,
+      "cab": 7
+    },
+    {
+      "id": 15,
+      "foodname": "Sweet Potato",
+      "calorie": 103,
+      "category": "Vegetable",
+      "protiens": 2,
+      "cab": 27
+    },
+    {
+      "id": 16,
+      "foodname": "Potato",
+      "calorie": 161,
+      "category": "Vegetable",
+      "protiens": 4.3,
+      "cab": 37
+    },
+    {"id": 17,
+      "foodname": "Carrot",
+      "calorie": 41,
+      "category": "Vegetable",
+      "protiens": 0.9,
+      "cab": 9
+    },
+    {
+      "id": 18,
+      "foodname": "Onion",
+      "calorie": 40,
+      "category": "Vegetable",
+      "protiens": 1.4,
+      "cab": 9
+    },
+    {
+      "id": 19,
+      "foodname": "Egg",
+      "calorie": 77,
+      "category": "Protein",
+      "protiens": 6.3,
+      "cab": 0.5
+    },
+    {
+      "id": 20,
+      "foodname": "Chicken Breast",
+      "calorie": 165,
+      "category": "Protein",
+      "protiens": 31,
+      "cab": 0
+    },
+    {
+      "id": 21,
+      "foodname": "Salmon",
+      "calorie": 206,
+      "category": "Protein",
+      "protiens": 22,
+      "cab": 0
+    },
+    {
+      "id": 22,
+      "foodname": "Tuna",
+      "calorie": 179,
+      "category": "Protein",
+      "protiens": 39,
+      "cab": 0
+    },
+    {
+      "id": 23,
+      "foodname": "Beef",
+      "calorie": 250,
+      "category": "Protein",
+      "protiens": 26,
+      "cab": 0
+    },
+    {
+      "id": 24,
+      "foodname": "Pork",
+      "calorie": 242,
+      "category": "Protein",
+      "protiens": 26,
+      "cab": 0
+    },
+    {
+      "id": 25,
+      "foodname": "Lamb",
+      "calorie": 294,
+      "category": "Protein",
+      "protiens": 25,
+      "cab": 0
+    },
+    {
+      "id": 26,
+      "foodname": "Shrimp",
+      "calorie": 85,
+      "category": "Protein",
+      "protiens": 20,
+      "cab": 0.2
+    },
+    {
+      "id": 27,
+      "foodname": "Quinoa",
+      "calorie": 120,
+      "category": "Grain",
+      "protiens": 4.4,
+      "cab": 21
+    },
+    {
+      "id": 28,
+      "foodname": "Brown Rice",
+      "calorie": 216,
+      "category": "Grain",
+      "protiens": 4.5,
+      "cab": 45
+    },
+    {
+      "id": 29,
+      "foodname": "Oats",
+      "calorie": 389,
+      "category": "Grain",
+      "protiens": 16.9,
+      "cab": 66
+    },
+    {
+      "id": 30,
+      "foodname": "Quinoa",
+      "calorie": 120,
+      "category": "Grain",
+      "protiens": 4.4,
+      "cab": 21
+    },
+    {
+      "id": 31,
+      "foodname": "Bread",
+      "calorie": 265,
+      "category": "Grain",
+      "protiens": 9.4,
+      "cab": 49
+    },
+    {
+      "id": 32,
+      "foodname": "Pasta",
+      "calorie": 131,
+      "category": "Grain",
+      "protiens": 5.5,
+      "cab": 26
+    },
+    {
+      "id": 33,
+      "foodname": "Milk",
+      "calorie": 60,
+      "category": "Dairy",
+      "protiens": 3.2,
+      "cab": 5.1
+    },
+    {
+      "id": 34,
+      "foodname": "Cheese",
+      "calorie": 402,
+      "category": "Dairy",
+      "protiens": 25,
+      "cab": 2.4
+    },
+    {
+      "id": 35,
+      "foodname": "Yogurt",
+      "calorie": 59,
+      "category": "Dairy",
+      "protiens": 3.5,
+      "cab": 5
+    },
+    {
+      "id": 36,
+      "foodname": "Butter",
+      "calorie": 717,
+      "category": "Dairy",
+      "protiens": 0.9,
+      "cab": 0.1
+    },
+    {
+      "id": 37,
+      "foodname": "Almonds",
+      "calorie": 579,
+      "category": "Nuts",
+      "protiens": 21,
+      "cab": 22
+    },
+    {
+      "id": 38,
+      "foodname": "Walnuts",
+      "calorie": 654,
+      "category": "Nuts",
+      "protiens": 15,
+      "cab": 14
+    },
+    {
+      "id": 39,
+      "foodname": "Peanuts",
+      "calorie": 567,
+      "category": "Nuts",
+      "protiens": 26,
+      "cab": 16
+    },
+    {
+      "id": 40,
+      "foodname": "Cashews",
+      "calorie": 553,
+      "category": "Nuts",
+      "protiens": 18,
+      "cab": 30
+    }
+  ];
+  
+
+
+
+
+
+  
+  
+  
+
+let vg=courses.filter(veg=>veg.category);
+
+let rr=document.querySelector('#Q1');
+  console.log(rr);
+  
+for(let value of vg){
+    
+    console.log(value.foodname);
+    let ne=document.createElement('li');
+    ne.textContent=value.foodname;
+    rr.appendChild(ne);
+}
+
+
+
+
+let vg1=courses.filter(veg=>veg.category=='Vegetable');
+
+let rr1=document.querySelector('#Q2');
+  console.log(rr1);
+  
+for(let value of vg1){
+    
+    console.log(value.foodname);
+    let ne=document.createElement('li');
+    ne.textContent=value.foodname;
+    rr1.appendChild(ne);
+}
+
+
+
+
+
+
+// let vg2=courses.filter(veg=>veg.category='Vegetable');
+let vg2=courses.filter(veg=>veg.category=='Fruit');
+
+let rr2=document.querySelector('#Q3');
+  console.log(rr2);
+  
+for(let value of vg2){
+    
+    console.log(value.foodname);
+    let ne=document.createElement('li');
+    ne.textContent=value.foodname;
+    rr2.appendChild(ne);
+}
+
+
+let vg3=courses.filter(veg=>veg.category=='Protein');
+
+let rr3=document.querySelector('#Q4');
+  console.log(rr3);
+  
+for(let value of vg3){
+    
+    console.log(value.foodname);
+    let ne=document.createElement('li');
+    ne.textContent=value.foodname;
+    rr3.appendChild(ne);
+}
+
+
+
+
+let vg4=courses.filter(veg=>veg.category=='Nuts');
+
+let rr4=document.querySelector('#Q5');
+  console.log(rr4);
+  
+for(let value of vg4){
+    
+    console.log(value.foodname);
+    let ne=document.createElement('li');
+    ne.textContent=value.foodname;
+    rr4.appendChild(ne);
+}
+
+
+
+let vg5=courses.filter(veg=>veg.category=='Grain');
+
+let rr5=document.querySelector('#Q6');
+  console.log(rr5);
+  
+for(let value of vg5){
+    
+    console.log(value.foodname);
+    let ne=document.createElement('li');
+    ne.textContent=value.foodname;
+    rr5.appendChild(ne);
+}
+
+
+
+let vg6=courses.filter(veg=>veg.category=='Dairy');
+
+let rr6=document.querySelector('#Q7');
+  console.log(rr6);
+  
+for(let value of vg6){
+    
+    console.log(value.foodname);
+    let ne=document.createElement('li');
+    ne.textContent=value.foodname;
+    rr6.appendChild(ne);
+}
+
+
+let vg7=courses.filter(veg=>veg.calorie>=100);
+
+let rr7=document.querySelector('#Q8');
+  console.log(rr7);
+  
+for(let value of vg7){
+    
+    console.log(value.foodname);
+    let ne=document.createElement('li');
+    ne.textContent=value.foodname;
+    rr7.appendChild(ne);
+}
+
+
+let vg8=courses.filter(veg=>veg.calorie<=100);
+
+let rr8=document.querySelector('#Q9');
+  console.log(rr8);
+  
+for(let value of vg8){
+    
+    console.log(value.foodname);
+    let ne=document.createElement('li');
+    ne.textContent=value.foodname;
+    rr8.appendChild(ne);
+}
+
+
+
+let vg9=courses.sort((a,b)=> b.protiens - a.protiens);
+
+let rr9=document.querySelector('#Q10');
+  console.log(rr9);
+  
+for(let value of vg9){
+    
+    console.log(value.foodname);
+    let ne=document.createElement('li');
+    ne.textContent=value.foodname;
+    rr9.appendChild(ne);
+}
+
+
+let vg10=courses.sort((a,b)=> a.protiens - b.protiens);
+
+let rr10=document.querySelector('#Q11');
+  console.log(rr10);
+  
+for(let value of vg10){
+    
+    console.log(value.foodname);
+    let ne=document.createElement('li');
+    ne.textContent=value.foodname;
+    rr10.appendChild(ne);
+}
+let cd=document.querySelectorAll('.st')
+
+cd.forEach(element => {
+  element.setAttribute('style','font-weight:600');
+})
